bfac4e4cf1371c45723ef7881db147ec02f50b57
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2024-12-15 14:38:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71, < 5.0.0"
+      version = ">= 4.14.0, < 5.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
+      version = ">= 3.6.3, < 4.0.0"
     }
   }
   backend "azurerm" {
